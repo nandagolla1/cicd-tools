8a93b39f5371072f334f98c4afa78defe7752278
maithili <maithiligolla@gmail.com>
2025-09-13 22:13:49 +0530

    cicd tools terraform

--- variables.tf
+++ variables.tf
@@ -8,12 +8,12 @@ variable "environment" {
 
 variable "zone_name" {
   type        = string
-  default     = "daws84s.site"
+  default     = "nanda.cyou"
   description = "description"
 }
 
 variable "zone_id" {
   type        = string
-  default     = "Z01894583A5LLRAMEST3A"
+  default     = "Z0241085GWXCWOXHL9YW"
   description = "description"
 }
